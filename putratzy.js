327cb07ed7c38645442613c77a9d9737af23510e
PutraWibu <putramodss@gmail.com>
2026-06-22 11:21:56 -0700

    Update putratzy.js

--- putratzy.js
+++ putratzy.js
@@ -66,7 +66,9 @@
             { title: "Sempurna - Gita Gutawa", artist: "JoooTezyh", src: "https://files.catbox.moe/yjm8dv.mp3", cover: "https://files.catbox.moe/xjl41p.jpg", category: "Pop/Sad Indo" },
             { title: "Serana - For Revenge", artist: "JoooTezyh", src: "https://files.catbox.moe/qpjpnu.mp3", cover: "https://files.catbox.moe/29h6zi.jpg", category: "Pop/Sad Indo" },
             { title: "Surat Cinta Untuk Starla - Virgoun", artist: "JoooTezyh", src: "https://files.catbox.moe/d65cfm.mp3", cover: "https://files.catbox.moe/639hz1.jpg", category: "Pop/Sad Indo" },
-            { title: "Tak Sanggup Lagi - Rossa", artist: "PutraMods", src: "https://files.catbox.moe/b5vyeu.mp3", cover: "https://files.catbox.moe/b5evoy.jpg", category: "Pop/Sad Indo" }
+            { title: "Tak Sanggup Lagi - Rossa", artist: "PutraMods", src: "https://files.catbox.moe/b5vyeu.mp3", cover: "https://files.catbox.moe/b5evoy.jpg", category: "Pop/Sad Indo" },
+            { title: "Cinta sedalam samudra - Putnkwn", artist: "Putnkwn", src: "https://files.catbox.moe/h4d15n.mp3", cover: "https://files.catbox.moe/7i8bbt.jpeg", category: "Pop/Sad Indo" },
+            { title: "Masih ingin Mencintaimu - Putnkwn", artist: "Putnkwn", src: "https://files.catbox.moe/b6ly9s.mp3", cover: "https://files.catbox.moe/2ml7ms.jpeg", category: "Pop/Sad Indo" }
         ];
 
         const playlist = _DATABASE_AUDIO_.sort((a, b) => a.title.localeCompare(b.title));
